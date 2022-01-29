78edea8f80f92df53bc5c15ffa8611c504fdd18b
Kateřina Dvořáková <kacule05@gmail.com>
2022-01-29 20:39:16 +0100

    Přidejte soubory projektu.

---
+++ 29.1.2022/29.1.2022.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    // Proměnná a je člen posloupnosti, d je deference, n je následující člen
+    int a, d, n = 1;
+    printf("Zadej clen posloupnosti: ");
+    scanf("%d", &a);
+    printf("Zadej diferenci: ");
+    scanf("%d", &d);
+    for (int i = 1; i <= 20; i++) {
+        n = a + (i - 1) * d;
+        printf("%2d. Cislo: %2d \n", i, n);
+    }
+}
